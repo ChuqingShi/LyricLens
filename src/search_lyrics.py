b456bfaf482b3b3c896faa8abe7773a87ffa8ce3
Chuqing Shi <chs139@ucsd.edu>
2026-09-03 18:22:24 -0700

    fix(data-hot100): deduplicate Hot 100 songs by song_id, add song_id to dataframes, and update config and affected files

--- src/search_lyrics.py
+++ src/search_lyrics.py
@@ -315,14 +315,9 @@ def main():
     # download_hot100()
     # hot100_song_df = clean_hot100_song()
 
-    from src.clean_hot100 import filter_hot100_song
-    from src.config import HOT100_SONG_OUTPUT, START_WEEK, END_WEEK
+    from src.config import HOT100_SONGS_OUTPUT
 
-    hot100_song_df = pd.read_parquet(HOT100_SONG_OUTPUT)
-
-    start_wk = START_WEEK
-    end_wk = END_WEEK
-    filtered_hot100_song_df = filter_hot100_song(hot100_song_df, start_wk=start_wk, end_wk=end_wk)
+    filtered_hot100_song_df = pd.read_parquet(HOT100_SONGS_OUTPUT)
     filtered_hot100_lyrics_df = generate_batch_lyrics(filtered_hot100_song_df)
 
 
